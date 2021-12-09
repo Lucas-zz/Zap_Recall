8baf09a9e6c0c35506dbb66eb7c4dadaeadecd17
Lucas <lucasazzollinivieira@gmail.com>
2021-12-09 00:31:07 -0300

    partial layout completed, success and failure screens still to be done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 export default function App() {
     return (
-        <>
-            <div>
-                <h1>HAHAHA</h1>
-            </div>
-        </>
+        <div>
+
+        </div>
     );
 }
