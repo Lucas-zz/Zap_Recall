4f2a1d9aadc7980684c3ee1ff1b82be82f7f3aa1
Eduardo Vedooto <vedootoeduardo@gmail.com>
2021-12-16 10:24:03 -0300

    Preparando revisão de código

--- src/Components/FlashcardScreen/Flashcards/TopCounter.js
+++
@@ -1,7 +0,0 @@
-export default function TopCounter({ number, total }) {
-    return (
-        <>
-            <span className="flashcard-top-number" data-identifier="counter">{number}/{total}</span>
-        </>
-    );
-}
